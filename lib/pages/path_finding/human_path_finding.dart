7a36e337ac84313d27b4d3250325fc3ce1d18b8c
Rubysand99 <hoangquan7b@gmail.com>
2026-08-16 22:40:16 +0700

    khung ban đầu từ Bonfire example

---
+++ lib/pages/path_finding/human_path_finding.dart
@@ -0,0 +1,20 @@
+import 'package:bonfire/bonfire.dart';
+import 'package:example/pages/player/simple/human.dart';
+
+class HumanPathFinding extends HumanPlayer with PathFinding, TapGesture {
+  HumanPathFinding({required Vector2 position}) : super(position: position) {
+    setupPathFinding(
+      pathLineStrokeWidth: 2,
+      useOnlyVisibleBarriers: false,
+    );
+  }
+
+  @override
+  void onTap() {}
+
+  @override
+  void onTapDownScreen(GestureEvent event) {
+    moveToPositionWithPathFinding(event.worldPosition);
+    super.onTapDownScreen(event);
+  }
+}
