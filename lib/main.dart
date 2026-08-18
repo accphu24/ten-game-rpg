f664d0ccff26a95c6fe18a827ef1b8996e83bdce
Rubysand99 <hoangquan7b@gmail.com>
2026-08-18 17:44:49 +0700

    feat: khoa man hinh ngang (landscape)

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'main_menu_screen.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.landscapeLeft,
+    DeviceOrientation.landscapeRight,
+  ]);
   runApp(const MyApp());
 }
 
