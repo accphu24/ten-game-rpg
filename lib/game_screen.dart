85d04944535e9c771ca444ebde5d651808a849f7
Rubysand99 <hoangquan7b@gmail.com>
2026-08-20 12:50:57 +0700

    debug: Flame toi gian tuyet doi, bo het logic game that

--- lib/game_screen.dart
+++ lib/game_screen.dart
@@ -1,18 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flame/game.dart';
+import 'package:flame/components.dart';
 
 class GameScreen extends StatelessWidget {
   const GameScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Colors.deepOrange,
-      body: Center(
-        child: Text(
-          'TEST MAN HINH GAME',
-          style: TextStyle(fontSize: 28, color: Colors.white),
-        ),
-      ),
-    );
+    return Scaffold(body: GameWidget(game: MinimalGame()));
+  }
+}
+
+class MinimalGame extends FlameGame {
+  @override
+  Color backgroundColor() => const Color(0xFF1a1a2e);
+
+  @override
+  Future<void> onLoad() async {
+    add(RectangleComponent(
+      position: Vector2(50, 50),
+      size: Vector2(100, 100),
+      paint: Paint()..color = const Color(0xFFFF0000),
+    ));
   }
 }
